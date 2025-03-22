9a9391afaf3d3edbf7a49d71be431a002a667fb7
darkjaffs <121581291+darkjaffs@users.noreply.github.com>
2025-03-22 18:44:02 +0500

    Add Filter Logic

--- main.py
+++ main.py
@@ -3,7 +3,23 @@ from bs4 import BeautifulSoup
 import html
 import csv
 
-URL = "https://weworkremotely.com/remote-jobs.rss"
+rss_feeds = {
+    "Customer Support": "https://weworkremotely.com/categories/remote-customer-support-jobs.rss",
+    "Product Jobs": "https://weworkremotely.com/categories/remote-product-jobs.rss",
+    "Full-Stack Programming": "https://weworkremotely.com/categories/remote-full-stack-programming-jobs.rss",
+    "Back-End Programming": "https://weworkremotely.com/categories/remote-back-end-programming-jobs.rss",
+    "Front-End Programming": "https://weworkremotely.com/categories/remote-front-end-programming-jobs.rss",
+    "All Programming": "https://weworkremotely.com/categories/remote-programming-jobs.rss",
+    "Sales and Marketing": "https://weworkremotely.com/categories/remote-sales-and-marketing-jobs.rss",
+    "Management and Finance": "https://weworkremotely.com/categories/remote-management-and-finance-jobs.rss",
+    "Design": "https://weworkremotely.com/categories/remote-design-jobs.rss",
+    "Devops and System Admin": "https://weworkremotely.com/categories/remote-devops-sysadmin-jobs.rss",
+    "Others": "https://weworkremotely.com/categories/all-other-remote-jobs.rss"
+}
+
+category = "Full-Stack Programming"
+
+URL = rss_feeds[category]
 
 response = request.get(URL)
 
@@ -11,7 +27,6 @@ rawhtml = response.text
 decoded_html = html.unescape(rawhtml)
 
 soup = BeautifulSoup(decoded_html, "html.parser")
-
 
 with open("page.html", "w", encoding="utf-8") as file:
     file.write(decoded_html)
