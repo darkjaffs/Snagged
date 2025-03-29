52173a0740a17d90d8e95ba674f4a5b072e970a1
darkjaffs <121581291+darkjaffs@users.noreply.github.com>
2025-03-29 15:11:00 +0500

    Complete Linking Frontend With Backend Database

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def home():
+    if request.method == "POST":
+        category = request.form["category"]
+        email = request.form["email"]
+
+        conn = sqlite3.connect("users.db")
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute("INSERT into subscribers (email, category) VALUES (?, ?)", (email, category))
+            conn.commit()
+        except sqlite3.IntegrityError:
+            pass
+        finally:
+            conn.close()
+
+        return redirect("/")
+
     return render_template("index.html")
 
-
-@app.route("/jobs", methods=["POST"])
-def jobs():
-    selected_category = request.form.get("category")
-    user_email = request.form.get("email")
-    return selected_category, user_email
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect("user.db")
+conn = sqlite3.connect("users.db")
 
 cursor =  conn.cursor()
 
