f8013d75c9526e45a5b2ae34b62899d25657df79
darkjaffs <121581291+darkjaffs@users.noreply.github.com>
2025-03-30 15:20:10 +0500

    Clean database.py

--- database.py
+++ database.py
@@ -1,18 +1,38 @@
 import sqlite3
 
-conn = sqlite3.connect("users.db")
+def create_connection():
+    """Establish a database connection and return the connection object."""
+    return sqlite3.connect("users.db")
 
-cursor =  conn.cursor()
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS subscribers (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT UNIQUE NOT NULL,
-    category TEXT NOT NULL,
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-)
-""")
 
-conn.commit()
+def create_tables():
+    
+    conn = create_connection()
+    cursor = conn.cursor()
 
-conn.close()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS jobs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        category TEXT NOT NULL,
+        url TEXT UNIQUE NOT NULL,
+        published_date DATETIME DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS subscribers (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email TEXT UNIQUE NOT NULL,
+        category TEXT NOT NULL,
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+    )
+    """)
+
+    conn.commit()
+
+    conn.close()
+
+if __name__ == "__main__":
+    create_tables()
